1563ddc11cbac01bb50da602f8e87a3eb3ab0e7c
⻩佳琪 <jq.huangbravesoft.co.jp>
2019-11-19 01:53:09 +0900

    Real World Vue finished.

---
+++ src/views/EventList.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1>Event Listing</h1>
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
+    <BaseIcon />
+  </div>
+</template>
+
+<script>
+import EventCard from '@/components/EventCard.vue'
+import EventService from '@/services/EventService.js'
+export default {
+  components: {
+    EventCard
+  },
+  data() {
+    return {
+      events: []
+    }
+  },
+  /*eslint no-console: ["error", { allow: ["warn", "error"] }] */
+  created() {
+    EventService.getEvents()
+      .then(reponse => {
+        this.events = reponse.data
+      })
+      .catch(error => {
+        console.error('There was an error:' + error.reponse)
+      })
+  }
+}
+</script>
